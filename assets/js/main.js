000340b6bae595c32cfe567c1946d4b69d25c7b2
Aftrskiyylol <dbb6743@gmail.com>
2025-08-24 23:08:45 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $(".cont_slick").slick({
+    dots: true, // Включение точек для навигации
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false, // Выключение стрелок
+  });
+});
